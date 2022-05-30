71b9207323505701a30e9ab6d272265ce8fed73f
Lars Kjær Thomsen <citybilen@gmail.com>
2022-05-30 17:00:20 +0200

    test

--- src/Automatron/TaskRunner.cs
+++ src/Automatron/TaskRunner.cs
@@ -180,8 +180,8 @@ namespace Automatron
             return await new AppRunner<BullseyeCommand>()
                 .Configure(c =>
                 {
-                    /*c.UseParameterResolver(_ => _bullseyeTargets);
-                    c.UseMiddleware(CreateController, MiddlewareStages.PostBindValuesPreInvoke);
+                    c.UseParameterResolver(_ => _bullseyeTargets);
+                    /*c.UseMiddleware(CreateController, MiddlewareStages.PostBindValuesPreInvoke);
                     c.UseMiddleware(BuildBullseyeTargets, MiddlewareStages.PostBindValuesPreInvoke);
                     c.BuildEvents.OnCommandCreated += AddControllerOptions;*/
                 })
