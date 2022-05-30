3c7f07c25c28747e3a27247995ee66bd03ce9ada
Lars Kjær Thomsen <citybilen@gmail.com>
2022-05-30 16:58:02 +0200

    test

--- src/Automatron.AzureDevOps/Generators/Models/AutomatronTask.cs
+++ src/Automatron.AzureDevOps/Generators/Models/AutomatronTask.cs
@@ -4,33 +4,9 @@ using YamlDotNet.Serialization;
 
 namespace Automatron.AzureDevOps.Generators.Models
 {
-    //public class AutomatronTask : Script
-    //{
-    //    public AutomatronTask(IJob job,string[] targets, bool skipDependencies=false, bool parallel=false) : base(job,$"dotnet run -- {Arguments(targets, skipDependencies, parallel)}")
-    //    {
-    //    }
-
-    //    private static string Arguments(string[] targets, bool skipDependencies, bool parallel)
-    //    {
-    //        var arguments = string.Join(" ", targets);
-
-    //        if (skipDependencies)
-    //        {
-    //            arguments += " -s";
-    //        }
-    //        if (parallel)
-    //        {
-    //            arguments += " -p";
-    //        }
-
-    //        return arguments;
-    //    }
-    //}
-
-    public class AutomatronTask : DotNetTask
+    public class AutomatronTask : Script
     {
-
-        public AutomatronTask(IJob job, string[] targets, bool skipDependencies = false, bool parallel = false) : base(job,new DotNetTaskInputs("run") { Arguments = Arguments(targets, skipDependencies, parallel) })
+        public AutomatronTask(IJob job, string[] targets, bool skipDependencies = false, bool parallel = false) : base(job, $"dotnet run -- {Arguments(targets, skipDependencies, parallel)}")
         {
         }
 
@@ -49,24 +25,48 @@ namespace Automatron.AzureDevOps.Generators.Models
 
             return arguments;
         }
-
-        [YamlIgnore]
-        public string? WorkingDirectory
-        {
-            get => Inputs!.WorkingDirectory;
-            set => Inputs!.WorkingDirectory = value;
-        }
-
-        [YamlIgnore]
-        public string[]? Secrets { get; set; }
-
-        //public IDictionary<string, object>? Env
-        //{
-        //    get
-        //    {
-        //        return Secrets?.ToDictionary(c => c, c => (object)$"$({c})") ?? _env;
-        //    }
-        //    set => _env = value;
-        //}
     }
+
+    //public class AutomatronTask : DotNetTask
+    //{
+
+    //    public AutomatronTask(IJob job, string[] targets, bool skipDependencies = false, bool parallel = false) : base(job,new DotNetTaskInputs("run") { Arguments = Arguments(targets, skipDependencies, parallel) })
+    //    {
+    //    }
+
+    //    private static string Arguments(string[] targets, bool skipDependencies, bool parallel)
+    //    {
+    //        var arguments = string.Join(" ", targets);
+
+    //        if (skipDependencies)
+    //        {
+    //            arguments += " -s";
+    //        }
+    //        if (parallel)
+    //        {
+    //            arguments += " -p";
+    //        }
+
+    //        return arguments;
+    //    }
+
+    //    [YamlIgnore]
+    //    public string? WorkingDirectory
+    //    {
+    //        get => Inputs!.WorkingDirectory;
+    //        set => Inputs!.WorkingDirectory = value;
+    //    }
+
+    //    [YamlIgnore]
+    //    public string[]? Secrets { get; set; }
+
+    //    //public IDictionary<string, object>? Env
+    //    //{
+    //    //    get
+    //    //    {
+    //    //        return Secrets?.ToDictionary(c => c, c => (object)$"$({c})") ?? _env;
+    //    //    }
+    //    //    set => _env = value;
+    //    //}
+    //}
 }
