6487cbb26817d3b968da86b9d2feb283e757c49f
Lars Kjær Thomsen <citybilen@gmail.com>
2022-05-31 10:07:43 +0200

    test

--- src/Automatron/BullseyeCommand.cs
+++ src/Automatron/BullseyeCommand.cs
@@ -6,16 +6,69 @@ using JetBrains.Annotations;
 
 namespace Automatron
 {
+    //internal class BullseyeCommand
+    //{
+    //    [DefaultCommand]
+    //    [UsedImplicitly]
+    //    public async Task Execute(
+    //        [Operand(Description = "A list of targets to run or list. If not specified, the \"default\" target will be run, or all targets will be listed.")]
+    //        string[]? targets,
+    //        [Option('c',Description = "Clear the console before execution")] 
+    //        bool? clear,
+    //        [Option('n',Description = "Do a dry run without executing actions")] 
+    //        bool? dryRun,
+    //        [Option('d',Description = "List all (or specified) targets and dependencies, then exit")]
+    //        bool? listDependencies,
+    //        [Option('i',Description = "List all (or specified) targets and inputs, then exit")]
+    //        bool? listInputs,
+    //        [Option('l',Description = "List all (or specified) targets, then exit")]
+    //        bool? listTargets,
+    //        [Option('t',Description = "List all (or specified) targets and dependency trees, then exit")]
+    //        bool? listTree,
+    //        [Option('p',Description = "Run targets in parallel")] 
+    //        bool? parallel,
+    //        [Option('s',Description = "Do not run targets' dependencies")]
+    //        bool? skipDependencies,
+    //        Targets bullseyeService,
+    //        IConsole console
+    //    )
+    //    {
+    //        var options = new Options
+    //        {
+    //            Clear = clear ?? false,
+    //            DryRun = dryRun ?? false,
+    //            Host = Host.Automatic,
+    //            ListDependencies = listDependencies ?? false,
+    //            ListInputs = listInputs ?? false,
+    //            ListTargets = listTargets ?? false,
+    //            ListTree = listTree ?? false,
+    //            NoColor = false,
+    //            Parallel = parallel ?? false,
+    //            SkipDependencies = skipDependencies ?? false,
+    //            Verbose = false
+    //        };
+
+    //        try
+    //        {
+    //            await bullseyeService.RunWithoutExitingAsync(targets ?? Enumerable.Empty<string>(), options, outputWriter: console.Out, diagnosticsWriter: console.Error);
+    //        }
+    //        catch (InvalidUsageException exception)
+    //        {
+    //            await console.Error.WriteLineAsync(exception.Message);
+    //        }
+    //    }
+    //}
+
     internal class BullseyeCommand
     {
         [DefaultCommand]
         [UsedImplicitly]
-        public async Task Execute(
+        public void Execute(
             [Operand(Description = "A list of targets to run or list. If not specified, the \"default\" target will be run, or all targets will be listed.")]
             string[]? targets,
-            [Option('c',Description = "Clear the console before execution")] 
+            [Option('c',Description = "Clear the console before execution")]
             bool? clear,
-            [Option('n',Description = "Do a dry run without executing actions")] 
+            [Option('n',Description = "Do a dry run without executing actions")]
             bool? dryRun,
             [Option('d',Description = "List all (or specified) targets and dependencies, then exit")]
             bool? listDependencies,
@@ -25,7 +78,7 @@ namespace Automatron
             bool? listTargets,
             [Option('t',Description = "List all (or specified) targets and dependency trees, then exit")]
             bool? listTree,
-            [Option('p',Description = "Run targets in parallel")] 
+            [Option('p',Description = "Run targets in parallel")]
             bool? parallel,
             [Option('s',Description = "Do not run targets' dependencies")]
             bool? skipDependencies,
@@ -50,12 +103,14 @@ namespace Automatron
 
             try
             {
-                await bullseyeService.RunWithoutExitingAsync(targets ?? Enumerable.Empty<string>(), options, outputWriter: console.Out, diagnosticsWriter: console.Error);
+                bullseyeService.RunWithoutExitingAsync(targets ?? Enumerable.Empty<string>(), options,
+                    outputWriter: console.Out, diagnosticsWriter: console.Error).Wait();
             }
             catch (InvalidUsageException exception)
             {
-                await console.Error.WriteLineAsync(exception.Message);
+                console.Error.WriteLine(exception.Message);
             }
         }
     }
+
 }
