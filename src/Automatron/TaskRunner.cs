1500d8ec9cbdee23fe34e3b1c398c57c95b59315
Lars Kjær Thomsen <citybilen@gmail.com>
2022-05-31 10:57:57 +0200

    test

--- src/Automatron/TaskRunner.cs
+++ src/Automatron/TaskRunner.cs
@@ -18,8 +18,7 @@ namespace Automatron
     {
         [DefaultCommand]
         public void Hest(
-            //[Operand(Description = "A list of targets to run or list. If not specified, the \"default\" target will be run, or all targets will be listed.")]
-            //[Option('t',Description = "A list of targets to run or list. If not specified, the \"default\" target will be run, or all targets will be listed.",Split = ' ')]
+            [Operand("targets",Description = "A list of targets to run or list. If not specified, the \"default\" target will be run, or all targets will be listed.")]
             IEnumerable<string> targets
             /*[Option('c',Description = "Clear the console before execution")]
             //bool? clear,
