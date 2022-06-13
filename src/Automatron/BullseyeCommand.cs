bdcd33c41ca39b50a1930e27f900cca809a01a06
Lars Kjær Thomsen <citybilen@gmail.com>
2022-06-13 11:36:58 +0200

    test

--- src/Automatron/BullseyeCommand.cs
+++ src/Automatron/BullseyeCommand.cs
@@ -23,14 +23,10 @@ namespace Automatron
         public async Task Execute(
             [Operand(Description = "A list of targets to run or list. If not specified, the \"default\" target will be run, or all targets will be listed.")]
             IEnumerable<string>? targets,
-            //[Option('c',Description = "Clear the console before execution")]
-            //bool? clear,
             [Option('n',Description = "Do a dry run without executing actions")]
             bool? dryRun,
             [Option('d',Description = "List all (or specified) targets and dependencies, then exit")]
             bool? listDependencies,
-            //[Option('i',Description = "List all (or specified) targets and inputs, then exit")]
-            //bool? listInputs,
             [Option('l',Description = "List all (or specified) targets, then exit")]
             bool? listTargets,
             [Option('t',Description = "List all (or specified) targets and dependency trees, then exit")]
