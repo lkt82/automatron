44c3d683353bf13828f6b627de8771fc5fdc9abe
Lars Kjær Thomsen <citybilen@gmail.com>
2022-06-14 09:17:23 +0200

    test

--- src/Automatron.AzureDevOps/Generators/Annotations/JobAttribute.cs
+++ src/Automatron.AzureDevOps/Generators/Annotations/JobAttribute.cs
@@ -2,34 +2,6 @@
 
 namespace Automatron.AzureDevOps.Generators.Annotations
 {
-
-    [AttributeUsage(AttributeTargets.Method)]
-    public class JobTemplateAttribute : Attribute
-    {
-        public string TemplateType { get; }
-
-        public string? Stage { get; }
-
-        public string? Name { get; set; }
-
-        public string? DisplayName { get; set; }
-
-        public string[]? DependsOn { get; set; }
-
-        public string? Condition { get; set; }
-
-        public JobTemplateAttribute(Type templateType)
-        {
-            TemplateType = templateType.FullName ?? throw new InvalidOperationException();
-        }
-
-        public JobTemplateAttribute(string stage, Type templateType) :this(templateType)
-        {
-            Stage = stage;
-        }
-
-    }
-
     [AttributeUsage(AttributeTargets.Method)]
     public class JobAttribute: Attribute
     {
