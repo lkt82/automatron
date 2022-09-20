0a73805d97669beb312986ca90df758e57acbf05
Lars Kjær Thomsen <citybilen@gmail.com>
2022-09-20 22:39:02 +0200

    test

--- src/Automatron.AzureDevOps/Generators/Models/AutomatronScript.cs
+++ src/Automatron.AzureDevOps/Generators/Models/AutomatronScript.cs
@@ -8,6 +8,6 @@ public sealed class AutomatronScript : Script
 
     private static string BuildCommand(string pipeline, string stage, string job, string step)
     {
-        return $"dotnet build -nologo -clp:NoSummary --verbosity quiet && dotnet run --no-build -- run --stage {stage} --job {job} --step {step} {pipeline}";
+        return $"dotnet build /nodeReuse:false /p:UseSharedCompilation=false -nologo -clp:NoSummary --verbosity quiet && dotnet run --no-build -- run --stage {stage} --job {job} --step {step} {pipeline}";
     }
 }
